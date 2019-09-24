92f7b74749ac7c3414eeff0a43cce8193c2f77d7
Mason Fidino <mfidino@lpzoo.org>
2019-09-24 13:02:06 -0500

    fixed site_names error

--- zooniverse_subject_uploadeR.R
+++ zooniverse_subject_uploadeR.R
@@ -207,7 +207,12 @@ get_sitenames <- function(file_paths = NULL, fileinfo = NULL){
     stop("please supply fileinfo to this function.")
   }
   if(!is.list(fileinfo) |
-     any(!names(fileinfo) %in% c('folder_to_upload', 'photo_file_type', 'search_subdirs', 'max_group'))){
+     any(
+       !c('folder_to_upload', 'photo_file_type',
+          'search_subdirs', 'max_group') %in%
+       names(fileinfo)
+     )
+  ){
     err <- paste0('Use get_fileinfo() to construct fileinfo argument.\n\n',
                   'EXAMPLE:\n\n',
                   "my_fileinfo <- get_fileinfo(folder_to_upload = 'file/path/to/photos/here',\n",
